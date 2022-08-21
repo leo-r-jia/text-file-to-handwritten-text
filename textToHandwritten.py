eabccda88e58eb7338996f807b0972916a949cd6
Leo Jia <leo.r.jia@outlook.com>
2022-08-21 22:10:41 +1200

    Added comments to textToHandwritten.py

--- textToHandwritten.py
+++ textToHandwritten.py
@@ -14,12 +14,14 @@ gap, ht = 0, 0
 
 last: str = ""
 
+# for each character in text file, paste the corresponding letter from myfont1 or myfont2 onto output image
 for i in txt.read().replace("\n", ""):
     if random.randint(0, 1) == 0:
         cases = Image.open("myfont1/{}.PNG".format(str(ord(i))))
     else:
         cases = Image.open("myfont2/{}.PNG".format(str(ord(i))))
 
+    # if nearing the end of the page, write on new line
     if sheet_width < gap or len(i)*38 > (sheet_width-gap):
         gap, ht = 0, ht+100
         if last != "\n" or last != " ":
@@ -34,4 +36,5 @@ for i in txt.read().replace("\n", ""):
 
     last = i
 
+# output image
 bg.show()
